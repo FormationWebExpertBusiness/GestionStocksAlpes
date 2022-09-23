771f8a088b696955ac7bf985d5d1c7b30f771e45
Enzo Neault <aza.woodyy@gmail.com>
2022-09-23 13:40:53 +0200

    Change style of the page + added possibility to delte an item

--- gestionStockAlpes/resources/views/displayStock.blade.php
+++ gestionStockAlpes/resources/views/displayStock.blade.php
@@ -1,6 +1,9 @@
 @extends('template')
 @section('title', 'Stock')
 @section('content')
+<div class="min-w-0 ml-64">
+    <livewire:filtres />
     <livewire:view-all :items="$items"/>
     <livewire:scripts />
+</div>
 @endsection
--- gestionStockAlpes/resources/views/livewire/view-all.blade.php
+++ gestionStockAlpes/resources/views/livewire/view-all.blade.php
@@ -58,21 +58,28 @@
                                             'brand' => $item->brand->name,
                                             'model' => $item->model,
                                             'quantity' => $item->quantity,
+                                            'comment' => $item->comment,
                                             'currency' => $item->currency,
                                             'unit' => $item->unit
                                         ])
                                         </td>
-                                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6">
-                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                        <td class="relative inline-flex justify-around min-w-full py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6">
+                                            <a href="#" class="text-indigo-600 hover:text-indigo-900"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                              <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                            </svg>
+                                            </a>
+                                            @livewire('delete-item', [
+                                              'itemId' => $item->id
+                                            ])
                                         </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
+                      </div>
                     </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    @livewireScripts
+              </div>
+              <br>
 </div>
