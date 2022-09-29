f75b2455a0864bcd0b4ece1a6be4e20c78f9da84
xenoword <mortier.moi91@gmail.com>
2022-09-29 09:53:30 +0200

    formulaire maj pour servir à l'update

--- gestionStockAlpes/resources/views/livewire/view-all.blade.php
+++ gestionStockAlpes/resources/views/livewire/view-all.blade.php
@@ -212,17 +212,8 @@
                                                     </td>
                                                 </div>
                                                 <div>
-                                                    <td
-                                                        class="relative inline-flex justify-around min-w-full py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6">
-                                                        <a href="{{ $item->model }}"
-                                                            class="text-indigo-600 hover:text-indigo-900">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5"
-                                                                stroke="currentColor" class="w-6 h-6">
-                                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                                            </svg>
-                                                        </a>
+                                                    <td class="relative inline-flex justify-around min-w-full py-4 pl-3 pr-4 text-center text-sm font-medium sm:pr-6">
+                                                        @livewire('item-form',['itemToUpdate' => $item])
                                                         <div>
                                                             <button wire:click="deleteItem({{$item->id}})" class="text-indigo-600 hover:text-indigo-900">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
