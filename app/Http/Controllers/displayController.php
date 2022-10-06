f0a697fc116ba08f61a007de241bd95f9721a67e
Enzo Neault <aza.woodyy@gmail.com>
2022-10-06 11:34:57 +0200

    :fire: Removed useless files and codes

--- app/Http/Controllers/displayController.php
+++ app/Http/Controllers/displayController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class displayController extends Controller
 {
-    public function displayItem(){
-        return view('displayItem');
-    }
-
     public function displayStock(){
         return view('displayStock');
     }
