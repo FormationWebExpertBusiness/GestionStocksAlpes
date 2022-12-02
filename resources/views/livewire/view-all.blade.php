1b3d9bf2c5421241ea7a5137b2c43a8af0603147
xenoword <mortier.moi91@gmail.com>
2022-12-02 09:09:18 +0100

    💄 change quantity column style

--- resources/views/livewire/view-all.blade.php
+++ resources/views/livewire/view-all.blade.php
@@ -167,17 +167,17 @@
                                                     $quantity = $commonItem->quantityOnRack($racksF, $rackLevelsF);
                                                 @endphp
                                                 <div class="w-4/6 inline-block">
-                                                    <div class="inline-block align-middle">
+                                                    <div class="inline-block align-middle font-bold text-base">
                                                         {{ $quantity }}
                                                         @if ($commonItem->quantity_urgent >= $quantity)
-                                                            <div class="inline-block rounded-full align-middle bg-red-200 text-red-800">
+                                                            <div class="inline-block rounded-full ml-4 align-middle bg-red-200 text-red-800">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                                                                 </svg>
 
                                                             </div>
                                                         @elseif ($commonItem->quantity_warning >= $quantity)
-                                                            <div class="inline-block rounded-full align-middle bg-orange-200 text-orange-800">
+                                                            <div class="inline-block rounded-full ml-4 align-middle bg-orange-200 text-orange-800">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                                                                 </svg>
