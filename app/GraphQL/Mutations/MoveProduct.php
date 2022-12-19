df4f64b616c73c1f352284544ca9a9f49ca74077
xenoword <mortier.moi91@gmail.com>
2022-12-19 14:01:50 +0100

    :alien: new mutation to move product

---
+++ app/GraphQL/Mutations/MoveProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Product;
+use App\Models\User;
+
+final class MoveProduct
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $product = Product::find($args['id']);
+        $product->rack_id = $args['rack_id'];
+        $product->rack_level = $args['rack_level'];
+        $product->mobileUser = User::find($args['user_id']);
+        $product->save();
+        
+        return $product;
+    }
+}
